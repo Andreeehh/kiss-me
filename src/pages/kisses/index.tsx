b4a8bc12066a0dbfdb0fed891a06b083b6cce2b9
André Carvalho <andre.vrlcarvalho@hotmail.com>
2024-10-06 22:05:58 -0300

    banco2

--- src/pages/kisses/index.tsx
+++ src/pages/kisses/index.tsx
@@ -1,56 +1,5 @@
-import { useEffect, useState } from 'react';
-import { db } from '../../firebase'; // Certifique-se de que o caminho está correto
-import { collection, getDocs } from 'firebase/firestore';
-import * as Styled from './styles';
+import Kisses from '../../templates/Kisses'; // Certifique-se de que o caminho está correto
 
-interface Kiss {
-  instagram: string;
-  image: string;
-  date: string;
-  time: string;
+export default function KissesPage() {
+  return <Kisses />;
 }
-
-function Kisses() {
-  const [kisses, setKisses] = useState<Kiss[]>([]);
-
-  useEffect(() => {
-    const fetchKisses = async () => {
-      const querySnapshot = await getDocs(collection(db, 'submissions'));
-      const kissesData: Kiss[] = [];
-
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        kissesData.push({
-          instagram: data.instagram,
-          image: data.image,
-          date: data.date,
-          time: data.time,
-        });
-      });
-
-      setKisses(kissesData);
-    };
-
-    fetchKisses();
-  }, []);
-
-  return (
-    <Styled.Wrapper>
-      <Styled.Title>Lista de Beijos</Styled.Title>
-      <Styled.List>
-        {kisses.map((kiss, index) => (
-          <Styled.ListItem key={index}>
-            <Styled.Info>
-              <p>
-                {kiss.instagram} beijou {kiss.image} no dia {kiss.date} às{' '}
-                {kiss.time}{' '}
-              </p>
-            </Styled.Info>
-          </Styled.ListItem>
-        ))}
-      </Styled.List>
-    </Styled.Wrapper>
-  );
-}
-
-export default Kisses;
